9c874a6e4e4651761887b834973707b50ed3c894
SRINATH-GITHU <dhumnorsrinath06052002@gmail.com>
2026-07-18 19:21:45 +0530

    test

---
+++ Demo1.java
@@ -0,0 +1,27 @@
+
+public class Demo1{
+    public static void main(String[] args){
+
+        int[] arr = {1,3,4,5,6};
+        arr[0] = 10;
+        // arr[5] =9;
+        
+        System.out.println(arr);
+
+        
+    }
+}
+
+/*
+Advantages
+
+Fast
+Simple
+
+Disadvantages
+
+Fixed size
+Cannot grow
+Many built-in operations are missing
+
+*/
