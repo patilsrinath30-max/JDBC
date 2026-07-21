9ad182c599fe1509c489a5be30aaad8c2e9d20d4
patilsrinath30-max <patilsrinath30@gmail.com>
2026-07-21 14:05:23 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,20 +1,62 @@
-package com.example.demo;
+package com.example.studentapi;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "students")
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String name;
 
-    public Student(int id, String name) {
-        this.id = id;
+    private String name;
+    private String branch;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, String branch, int age) {
         this.name = name;
+        this.branch = branch;
+        this.age = age;
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
